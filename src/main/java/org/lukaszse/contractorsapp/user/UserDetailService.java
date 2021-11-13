16d82209701dcab67c98277e621fd7c741249d3e
lukaszse <llseremak@gmail.com>
2021-11-13 11:54:12 +0100

    Add user repository

---
+++ src/main/java/org/lukaszse/contractorsapp/user/UserDetailService.java
@@ -0,0 +1,27 @@
+package org.lukaszse.contractorsapp.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.lukaszse.contractorsapp.user.DTO.MyUserPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUserName(username)
+                .map(MyUserPrincipal::new)
+                .map(myUserPrincipal -> (UserDetails) myUserPrincipal)
+                .orElseThrow(() -> new UsernameNotFoundException("User with userName=%s cannot be found".formatted(username)));
+    }
+}
--- src/main/resources/db/migration/V6__Add_user_table.sql
+++ src/main/resources/db/migration/V6__Add_user_table.sql
@@ -1,10 +1,12 @@
-create table users (
+create table users
+(
 
-    user_name varchar(20) primary key,
+    user_name  varchar(20) primary key,
     first_name varchar(100),
-    last_name varchar(100),
-    password varchar(200)
+    last_name  varchar(100),
+    password   varchar(200)
 );
 
-insert into users (user_name, first_name, last_name, password)
-                        values ('admin', 'John', 'Doe', 'admin');
+insert into users (user_name, first_name, last_name, password) values ('admin', 'John', 'Doe', 'admin');
+
+insert into users (user_name, first_name, last_name, password) values ('james', 'James', 'Smith', '12345');
